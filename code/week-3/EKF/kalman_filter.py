22e3bdd9a25b97c832fc0589feb63f033401699a
leeyell <lylim5050@kookmin.ac.kr>
2021-04-08 15:21:33 +0900

    Assignment-2

--- code/week-3/EKF/kalman_filter.py
+++ code/week-3/EKF/kalman_filter.py
@@ -5,7 +5,7 @@ from tools import Jacobian
 
 class KalmanFilter:
     def __init__(self, x_in, P_in, F_in, H_in, R_in, Q_in):
-        self.x = x_in
+        self.x = x_in       # x = [px, py, vx, vy]
         self.P = P_in
         self.F = F_in
         self.H = H_in
@@ -23,13 +23,29 @@ class KalmanFilter:
         self.x = self.x + np.dot(K, z - np.dot(self.H, self.x))
         self.P = self.P - np.dot(np.dot(K, self.H), self.P)
 
+    # 입력으로 받는 z = [rho, phi, rho_dot]
+    # 이 함수가 호출될 때는 외부에서 이미 저 위의 predict() 함수가 실행된 상태이다.
+    # 즉, x -> x', P -> P'으로 업데이트 된 상태.
     def update_ekf(self, z):
-        # TODO: Implement EKF update for radar measurements
+        def h(x):
+            rho = sqrt(x[0]**2 + x[1]**2)            
+            phi = atan2(x[1], x[0])
+            rho_dot = (x[0]*x[2] + x[1]*x[3]) / rho
+
+            return np.array([rho, phi, rho_dot])
+
         # 1. Compute Jacobian Matrix H_j
+        H_j = Jacobian(self.x)
         # 2. Calculate S = H_j * P' * H_j^T + R
-        # 3. Calculate Kalman gain K = H_j * P' * Hj^T + R
+        S = np.dot(np.dot(H_j, self.P), H_j.T) + self.R
+        # 3. Calculate Kalman gain K = P' * Hj^T * S^-1
+        K = np.dot(np.dot(self.P, H_j.T), np.linalg.inv(S))
         # 4. Estimate y = z - h(x')
+        y = z - h(self.x)
         # 5. Normalize phi so that it is between -PI and +PI
+        y[1] = y[1] % np.pi if y[1] > 0 else -(-y[1] % np.pi)
         # 6. Calculate new estimates
         #    x = x' + K * y
         #    P = (I - K * H_j) * P
+        self.x = self.x + np.dot(K, y)
+        self.P = np.dot((np.identity(4) - np.dot(K, H_j)), self.P)
--- code/week-3/README.md
+++ code/week-3/README.md
@@ -52,3 +52,43 @@ The program consists of five modules:
 ### Assignment
 
 Complete the implementation of EKF with sensor fusion by writing the function `update_ekf()` in the module `kalman_filter`. Details are given in class and instructions are included in comments.
+
+***
+
+## Report
+
+#### update_ekf
+KalmanFilter 클래스 객체의 멤버 함수이며, 다음과 같은 parameter를 input으로 받는다.
+__Input__
+* z: [rho, phi, rho_dot]. Radar 센서의 measurement 값이다. main으로 실행되는 함수로부터 입력받는다.
+
+매 time t에서의 measurement가 입력으로 들어올 때마다 위의 파라미터를 통해 클래스 객체가 가지고 있는 x (px, py, vx, vy)와 P를 업데이트한다.
+
+```
+# 입력으로 받는 z = [rho, phi, rho_dot]
+# 이 함수가 호출될 때는 외부에서 이미 저 위의 predict() 함수가 실행된 상태이다.
+# 즉, x -> x', P -> P'으로 업데이트 되어 있음.
+def update_ekf(self, z):
+    def h(x):
+        rho = sqrt(x[0]**2 + x[1]**2)            
+        phi = atan2(x[1], x[0])
+        rho_dot = (x[0]*x[2] + x[1]*x[3]) / rho
+
+        return np.array([rho, phi, rho_dot])
+
+    # 1. Compute Jacobian Matrix H_j
+    H_j = Jacobian(self.x)
+    # 2. Calculate S = H_j * P' * H_j^T + R
+    S = np.dot(np.dot(H_j, self.P), H_j.T) + self.R
+    # 3. Calculate Kalman gain K = P' * Hj^T * S^-1
+    K = np.dot(np.dot(self.P, H_j.T), np.linalg.inv(S))
+    # 4. Estimate y = z - h(x')
+    y = z - h(self.x)
+    # 5. Normalize phi so that it is between -PI and +PI
+    y[1] = y[1] % np.pi if y[1] > 0 else -(-y[1] % np.pi)
+    # 6. Calculate new estimates
+    #    x = x' + K * y
+    #    P = (I - K * H_j) * P
+    self.x = self.x + np.dot(K, y)
+    self.P = np.dot((np.identity(4) - np.dot(K, H_j)), self.P)
+```
